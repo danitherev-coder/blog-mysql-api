928e22d0e33a83b30ab309aefdb0ad425127b464
Carlos Daniel <danitherev@pop-os.localdomain>
2022-12-26 11:18:40 -0500

    xdxdas

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(cors({
 }))
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://hilarious-cobbler-0478cd.netlify.app");
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
