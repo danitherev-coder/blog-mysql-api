1db3c71c7b603d4248a96327a9e86f6bc28222d3
Carlos Daniel <danitherev@pop-os.localdomain>
2022-12-25 22:25:58 -0500

    agregado api 22

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 import mysql from "mysql"
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   port: process.env.DB_PORT,
 })
+
